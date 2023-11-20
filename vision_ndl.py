497fbfc3a47a8d2ffad4e9399e459d1c7fa1e6bd
dtcurragh <97108312+dtcurragh@users.noreply.github.com>
2023-11-20 14:40:08 +0000

    add print statements to assist with debugging

--- vision_ndl.py
+++ vision_ndl.py
@@ -1,5 +1,3 @@
-# This script will attempt to extract names dates and locations for each memorial_number
-
 import os
 import json
 import base64
@@ -12,6 +10,7 @@ import glob
 def encode_image(image_path):
     try:
         with open(image_path, "rb") as image_file:
+            print(f"Encoding image: {image_path}")
             return base64.b64encode(image_file.read()).decode('utf-8')
     except FileNotFoundError:
         print(f"Error: The file '{image_path}' was not found.")
@@ -22,6 +21,7 @@ def encode_image(image_path):
 
 
 def process_images(image_paths, api_key, output_directory, batch_number):
+    print(f"Starting to process images in batch {batch_number}...")
     headers = {
         "Content-Type": "application/json",
         "Authorization": f"Bearer {api_key}"
@@ -33,7 +33,7 @@ def process_images(image_paths, api_key, output_directory, batch_number):
             "content": [
                 {
                     "type": "text",
-                    "text": "You're an expert in OCR and are working in a heritage/genealogy context assisting in data processing post graveyard survey.Examine these images and extract the names,dates and suspected location names for each memorial number-no other fields..Respond in json format only.e.g {memorial_number: 69, name: John Doe, date: Jan 1, 1800, location: Springfield}. If no memorial number,name, date or location is visible in an image,return a json with NULL in each field"
+                    "text": "You're an expert in OCR and are working in a heritage/genealogy context assisting in data processing post graveyard survey..."
                 }
             ]
         }
@@ -52,13 +52,13 @@ def process_images(image_paths, api_key, output_directory, batch_number):
         "max_tokens": 1500  # Adjust based on requirements and token limits
     }
 
+    print("Sending request to OpenAI...")
     try:
         response = requests.post(
             "https://api.openai.com/v1/chat/completions", headers=headers, json=payload)
         response.raise_for_status()
 
-        # Handle response for each image separately if needed
-        # For simplicity, saving one combined output file for the batch
+        print("Received response from OpenAI, writing to file...")
         output_file_name = f"output_batch_{batch_number}.json"
         output_path = os.path.join(output_directory, output_file_name)
 
@@ -80,8 +80,13 @@ def process_images(image_paths, api_key, output_directory, batch_number):
 
 
 def main(folder_path, api_key, output_directory):
+    print(f"Searching for image files in {folder_path}...")
     image_files = glob.glob(os.path.join(folder_path, '*.jpg'))
     batch_size = 3
+
+    if not image_files:
+        print("No JPG files found in the specified directory.")
+        return
 
     for i in range(0, len(image_files), batch_size):
         batch = image_files[i:i + batch_size]
@@ -97,8 +102,11 @@ if __name__ == "__main__":
 
     folder_path = sys.argv[1]
     api_key = os.getenv('OPENAI_API_KEY')
-    # Change to your output directory path
-    output_directory = "PATH_TO_OUTPUT_JSONS_FOLDER"
+    if not api_key:
+        print("Error: OPENAI_API_KEY not set.")
+        sys.exit(1)
+    output_directory = "PATH_TO_OUTPUT_FOLDER"
+    print("Starting the image processing script...")
     main(folder_path, api_key, output_directory)
 
-# To run this script, type 'python3 vision-v4.py {{path/to/input/folder}}' in terminal
+# To run this script, type 'python3 vision_ndl.py <path/to/input/folder>' in terminal
--- vision_transcript.py
+++ vision_transcript.py
@@ -1,5 +1,3 @@
-# This script will attempt to extract the handwritten inscription text for each memorial_number
-
 import os
 import json
 import base64
@@ -12,6 +10,7 @@ import glob
 def encode_image(image_path):
     try:
         with open(image_path, "rb") as image_file:
+            print(f"Encoding image: {image_path}")
             return base64.b64encode(image_file.read()).decode('utf-8')
     except FileNotFoundError:
         print(f"Error: The file '{image_path}' was not found.")
@@ -22,6 +21,7 @@ def encode_image(image_path):
 
 
 def process_images(image_paths, api_key, output_directory, batch_number):
+    print(f"Starting to process images in batch {batch_number}...")
     headers = {
         "Content-Type": "application/json",
         "Authorization": f"Bearer {api_key}"
@@ -33,7 +33,7 @@ def process_images(image_paths, api_key, output_directory, batch_number):
             "content": [
                 {
                     "type": "text",
-                    "text": "You're an expert in OCR and are working in a heritage/genealogy context assisting in data processing post graveyard survey.Examine these images and extract the handwritten text from the inscription field for each memorial number-no other fields..Respond in json format only.e.g {memorial_number: 69, inscription: SACRED HEART OF JESUS HAVE MERCY ON THE SOUL OF THOMAS RUANE LISNAGROOBE WHO DIED APRIL 16th 1923 AGED 74 YRS AND OF HIS WIFE MARGARET RUANE DIED JULY 26th 1929 AGED 78 YEARS R. I. P .ERECTED BY THEIR FOND SON THOMAS RUANE PHILADELPHIA USA}. transcription fields should be all one line. If no memorial number or inscription is visible in an image,return a json with NULL in each field"
+                    "text": "You're an expert in OCR and are working in a heritage/genealogy context assisting in data processing post graveyard survey..."
                 }
             ]
         }
@@ -52,13 +52,13 @@ def process_images(image_paths, api_key, output_directory, batch_number):
         "max_tokens": 1500  # Adjust based on requirements and token limits
     }
 
+    print("Sending request to OpenAI...")
     try:
         response = requests.post(
             "https://api.openai.com/v1/chat/completions", headers=headers, json=payload)
         response.raise_for_status()
 
-        # Handle response for each image separately if needed
-        # For simplicity, saving one combined output file for the batch
+        print("Received response from OpenAI, writing to file...")
         output_file_name = f"output_batch_{batch_number}.json"
         output_path = os.path.join(output_directory, output_file_name)
 
@@ -80,8 +80,13 @@ def process_images(image_paths, api_key, output_directory, batch_number):
 
 
 def main(folder_path, api_key, output_directory):
+    print(f"Searching for image files in {folder_path}...")
     image_files = glob.glob(os.path.join(folder_path, '*.jpg'))
     batch_size = 3
+
+    if not image_files:
+        print("No JPG files found in the specified directory.")
+        return
 
     for i in range(0, len(image_files), batch_size):
         batch = image_files[i:i + batch_size]
@@ -97,8 +102,12 @@ if __name__ == "__main__":
 
     folder_path = sys.argv[1]
     api_key = os.getenv('OPENAI_API_KEY')
+    if not api_key:
+        print("Error: OPENAI_API_KEY not set.")
+        sys.exit(1)
     # Change to your output directory path
-    output_directory = "PATH_TO_OUTPUT_JSON_FOLDER"
+    output_directory = "PATH_TO_OUTPUT_FOLDER"
+    print("Starting the image processing script...")
     main(folder_path, api_key, output_directory)
 
-# To run this script, type 'python3 vision-v4.py {{path/to/input/folder}}' in terminal
+# To run this script, type 'python3 vision-v4.py <path/to/input/folder>' in terminal
