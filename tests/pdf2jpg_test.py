35dd1cea323f539abf71327bbab81e63779c8bf5
dtcurragh <97108312+dtcurragh@users.noreply.github.com>
2023-11-29 20:47:19 +0000

    add gitignore

--- tests/pdf2jpg_test.py
+++ tests/pdf2jpg_test.py
@@ -8,8 +8,8 @@ from pdf2jpg import convert_pdf_to_jpg
 def setup_pdf_and_output():
     # Setup a test PDF path and output folder
     # Ensure you have a test PDF at this location
-    pdf_path = '/Users/danieltierney/Desktop/HistoricGraves/OLCWS_Mitchelstown/OLCWS_Mitchelstown__allsheets.pdf'
-    output_folder = '/Users/danieltierney/Desktop/Dev/openai-playground/HG_TextHarvest_v1/test_folder/json_outputs'
+    pdf_path = 'PATH_TO_PDF'
+    output_folder = 'PATH_TO_OUTPUT_FOLDER'
     yield pdf_path, output_folder
     # Teardown after tests
     if os.path.exists(output_folder):
