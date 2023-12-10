7fed69918a33c9030253d4a30a1a8848a0c53325
dtcurragh <97108312+dtcurragh@users.noreply.github.com>
2023-12-10 22:04:39 +0000

    add validat.py
    first phase of data validation

---
+++ validate.py
@@ -0,0 +1,60 @@
+import json
+from jsonschema import validate
+from jsonschema.exceptions import ValidationError
+
+# Define the JSON schema
+schema = {
+    "type": "array",
+    "items": {
+        "type": "object",
+        "properties": {
+            # Allows integers or null
+            "memorial_number": {"type": ["integer", "null"]},
+            # Allows strings or null
+            "inscription": {"type": ["string", "null"]},
+        },
+        "required": ["memorial_number", "inscription"]
+    }
+}
+
+
+def validate_json(json_data, schema):
+    try:
+        validate(instance=json_data, schema=schema)
+        print("Validation successful.")
+    except ValidationError as ve:
+        print("Validation error:", ve)
+        return False
+    return True
+
+
+def extract_json_content(file_data):
+    try:
+        # Extract the JSON array from the nested structure
+        content = file_data['choices'][0]['message']['content']
+        content = content.replace('```json\n', '').replace('\n```', '').strip()
+        return json.loads(content)
+    except (KeyError, json.JSONDecodeError) as e:
+        print(f"Error extracting JSON content: {e}")
+        return None
+
+
+def main(json_file_path):
+    try:
+        with open(json_file_path, 'r') as file:
+            file_data = json.load(file)
+            json_data = extract_json_content(file_data)
+            if json_data is not None and validate_json(json_data, schema):
+                print("JSON file is valid according to the schema.")
+            else:
+                print("JSON file is not valid.")
+    except FileNotFoundError:
+        print(f"File not found: {json_file_path}")
+    except json.JSONDecodeError as e:
+        print(f"Error reading JSON file: {e}")
+
+
+if __name__ == "__main__":
+    # Replace with the path to your JSON file
+    json_file_path = '/Users/danieltierney/Desktop/HistoricGraves/Kiltullagh_Roscommon/kil_output_jsons/output_batch_1.json'
+    main(json_file_path)
